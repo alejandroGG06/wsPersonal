18b3853f10e25d498308598e740934a159aa4128
Alejandro <grajalesalejandro068@gail.com>
2024-03-12 22:44:46 +0100

    Actividad 20

---
+++ Actividad20/src/Coche2/Maincoche2.java
@@ -0,0 +1,93 @@
+package Coche2;
+
+import java.util.ArrayList;
+
+import java.util.Scanner;
+
+public class Maincoche2 {
+	Scanner scan = new Scanner(System.in);
+	ArrayList<coche> listacoches = new ArrayList<>();
+
+	public static void main(String[] args) {
+		Maincoche2 main= new Maincoche2();
+		main.arrancarPrograma();
+	
+		
+	}
+	
+
+	
+	public void arrancarPrograma() {
+		int opcion = 0;
+		do {
+			System.out.println("seleccione una de estas 5 opciones");
+			System.out.println("1. crear coche");
+			System.out.println("2. mostrar coche");
+			System.out.println("3. mostrar contador de ID");
+			System.out.println("4. Resetear contador ID");
+			System.out.println("5. Salir del programa");
+
+			opcion = scan.nextInt();
+
+			switch (opcion) {
+			case 1:
+				crearCoche();
+
+				break;
+			case 2:
+				mostrarCoche();
+
+				break;
+			case 3:
+				mostarCont();
+
+				break;
+
+			case 4:
+				coche.resetCont();
+				break;
+
+			case 5:
+				System.out.println("progrrama finalizado");
+				break;
+
+			default:
+				System.out.println("valor no valido, intentelo nuevamente");
+				break;
+			}
+
+		} while (opcion != 5);
+
+	}
+
+	public void crearCoche() {
+		
+		coche Coche = new coche();
+		System.out.println("introduzca la matricula");
+		Coche.matricula = scan.next();
+
+		System.out.println("las marcas que tenemos son: " + coche.RENAULT +" "+ coche.TOYOTA);
+		System.out.println("escoge una opcion");
+		Coche.marca = scan.next().toUpperCase();
+		
+		listacoches.add(Coche);
+		
+		
+
+		
+	}
+
+	public void mostrarCoche() {
+		for (coche Coche : listacoches) {
+			System.out.println(Coche.toString());
+		}
+
+		}
+		
+	
+	 public void mostarCont() {
+		 System.out.println("el id actual es: "+ coche.getContadorID());
+	 }
+	
+
+}
---
+++ Actividad20/src/Coche2/coche.java
@@ -0,0 +1,54 @@
+package Coche2;
+
+public class coche {
+
+	public int id;
+	public String matricula;
+	public String marca;
+	public static int contadorID;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getMatricula() {
+		return matricula;
+	}
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	public static int getContadorID() {
+		return contadorID;
+	}
+	public static void setContadorID(int contadorID) {
+		coche.contadorID = contadorID;
+	}
+	
+	@Override
+	public String toString() {
+		return "coche [id=" + id + ", matricula=" + matricula + ", marca=" + marca + "]";
+	}
+	
+	public coche() {
+		this.id=contadorID++;
+
+	}
+	
+	public static void resetCont(){
+		contadorID=0;
+	}
+	
+	
+	public static final String TOYOTA="Toyota";
+	public static final String RENAULT="Renault";
+	
+	
+}
